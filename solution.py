5366952199b538f1441f1ffe0199858113b78181
avfst <105452537+avfst@users.noreply.github.com>
2023-04-13 23:49:22 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,24 +2,22 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 297211248 # Ваш chat ID, не меняйте название переменной
+chat_id = 297211248 
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+
     conv1=x_success/x_cnt
     conv2=y_success/y_cnt
     conv_delta=conv2-conv1
     delta=2.58*((conv1*(1-conv1)/x_cnt) + (conv2*(1-conv2)/y_cnt))**0.5
     mn= conv_delta - delta
     mx=conv_delta + delta
-    if (mx*mn)<0:
-        res=False
-    else:
+    if (mx*mn)>0 and conv2>conv1:
         res=True
+    else:
+        res=False
         
-    return res # Ваш ответ, True или False
+    return res 
